4e585e031341f98d09abcdbf0583d312e9d42511
Harrison-Berek <hcberek@gmail.com>
2021-03-08 15:56:58 -0700

    Add title, message, initial deal messages, placeBet function

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ const ranks = ['02', '03', '04', '05', '06', '07', '08',
 
 
 /*----- app's state (variables) -----*/
-let chips, winner, playerCards, dealerCards, turn; 
+let playerChips, winner, playerCards, dealerCards, turn, bet; 
 
 /*----- cached element references -----*/
-// const msgEl = document.getElementById('msg');
+const msgEl = document.getElementById('msg');
  
 /*----- event listeners -----*/
 
@@ -19,16 +19,18 @@ document.querySelector('#hit').addEventListener('click', handleHit);
 document.querySelector('#stand').addEventListener('click', handleStand);
 document.querySelector('#double-down').addEventListener('click', handleDouble);
 document.querySelector('#replay').addEventListener('click', init);
+document.querySelector("#betBtn").addEventListener('click', placeBet);
 // document.querySelector('#split').addEventListener('click', handleSplit);
 
 
 /*----- functions -----*/
 function init() {
-    chips = 1000;
+    playerChips = 100;
     turn = 1; 
     deck = shuffledDeck();
     playerCrads = [];
     dealerCards = [];
+    bet = 0;
     winner = null; 
     render();
 };
@@ -59,6 +61,36 @@ function shuffledDeck() {
 init();
 console.log(deck);
 
+function placeBet() {
+   let minBet = 50; 
+   bet = prompt(`Your chip total is ${playerChips}. Minimum bet is ${minBet}. Please place your bet:`)
+   while (bet > 1000 - playerChips || bet < 50) {
+    bet = prompt(`Sorry, bet must be between ${1000 - playerChips} and ${minBet}. Please place your bet:`)
+   } 
+   playerChips -= bet;
+   initDeal();
+   return bet; 
+};
+
+console.log(bet);
+
+function initDeal() {
+    playerCards = [deck.pop(), deck.pop()];
+    dealerCards = [deck.pop()];
+    if (playerCards[0].value + playerCards[1].value === 21 && dealerCards[0].value
+        + dealerCards[1].value !== 21) {
+        msgEl.innerHTML = `Congratulations you've got BLACKJACK!`
+        // playerChips = 
+    } else if (playerCards[0].value + playerCards[1].value === 21 && dealerCards[0].value
+        + dealerCards[1].value === 21) {
+        msgEl.innerHTML = `Push! You didn't win but you didn't lose`;
+    } else {
+        msgEl.innerHTML = `You have ${playerCards[0].value + playerCards[1].value} would you like to Hit, Stand or  Double-Down`;
+    }
+};
+
+
+
 // function dealCard() {
 //     if (turn === 1 )
 // };
@@ -80,5 +112,13 @@ function getWinner() {
 };
 
 function render() {
-
+    //Show correct message
+        // if bet 0 "Welcome to BlackJack! Please place your bet."
+        if (bet === 0){
+            msgEl.innerHTML = `Please place your bet to get starter.`
+        } 
+        // else if ()     
+    //display correct buttons 
+    //update bet and playerChips
+    //deal cards
 };
